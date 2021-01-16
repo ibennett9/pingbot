7fbc2bb95965e3671e26bd7f2929becaeabb81f6
ibennett9 <60464259+ibennett9@users.noreply.github.com>
2021-01-16 18:53:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on("message", (message) => {
 message.channel.send(`${command}`);
   switch (command){
       case "troll":
-          message.send('Command recieved');
+          message.channel.send('Command recieved');
           if (!args[0].mentions.users.first()) return;
           let user = args[0].mentions.users.first();
           message.channel.send(`You want to ping <@${user.id}`);
