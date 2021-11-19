778d6b832702b47bdd11e6aa9dff3a0d5df89496
ibennett9 <60464259+ibennett9@users.noreply.github.com>
2021-11-19 16:28:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ client.on('ready', () => {
 const prefix = "!";
 client.on("message", (message) => {
   // Exit and stop if it's not there
+  if (message.content.includes("comrade")) {heraldTheComrade(message); return;}
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.slice(prefix.length).trim().split(/ +/g);
   var command = args.shift().toLowerCase();
@@ -19,21 +20,23 @@ client.on("message", (message) => {
 case "pog":
           message.reply('You are the goat :star_struck: :star_struck:');
           break;
-
   }
   
 });
-
 client.login(process.env.BOT_TOKEN);
-
 function troll(message){
   
     var args = message.content.slice(prefix.length).trim().split(/ +/g);
     var userPerson = message.mentions.users.first();
+    if (userPerson.id == 277580041053798401)
+    {
+        message.channel.send(`You can't ping our mighty saviour Lord DinoReaper! That is an illegal move!`);
+        return;
+    }
     args.shift();
     args.shift();
     var repeat = Number(args.shift());
-    var interval = Number(args.shift());
+    var interval = args.shift();
           if(!Number.isInteger(repeat)) return;
           repeat = Math.abs(repeat);
           if (repeat > 50) 
@@ -45,15 +48,15 @@ function troll(message){
             message.channel.send(`You want to ping <@${userPerson.id}> ${repeat} times`);
             for (var i = 0; i<repeat; i++)
             {
-                message.channel.send(`<@${userPerson.id}>`);
+                message.channel.send(`<@${userPerson.id}> ${interval}`);
             }
-        } else {
-          interval = Math.abs(interval);
-          for(var i =0;i<repeat;i++)
-          {
-            setTimeout(function(){message.channel.send(`<@${userPerson.id}>`);}, interval);
-          }
         }
+}
+function heraldTheComrade(message)
+{
+    var user = message.author;
+    message.channel.send(`<@${user.id}>, you called upon your Comrade friends! I am here to support you!`);
+    message.channel.send(`https://c.tenor.com/rH_kpMNuotUAAAAM/communism-communist.gif`);
 }
 
 function foo(message){
